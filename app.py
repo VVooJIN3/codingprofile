80758b81d21ab49df1854bb2d542002da4432a05
VVooJIN3 <131952511+VVooJIN3@users.noreply.github.com>
2024-05-26 18:39:06 +0900

    Update app.py
    주석 제거

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ def choipage():
    return render_template('choi.html')
 
 
-#########################################################################################################
 # 조우진 : 방명록 수정, 삭제하기
 
 #방명록 삭제하기
@@ -103,75 +102,6 @@ def guestbook_update():
 
             return jsonify({'result' : 'success', 'msg' : '수정이 완료되었습니다'})
             
-# #방명록 불러오기 
-# @app.route('/guestbook',methods=['GET'])
-# def guestbook_get():
-#     name_give = request.args.get('name',"")
-#     print(name_give)
-#     all_guestbook = list(db.guestbook.find({"name":name_give}))    
-    
-#     for guestbook in all_guestbook:#_id값을 timestmp로 변경하여 html에 전달
-#         objectid = guestbook["_id"]
-#         guestbook["_id"]=str(guestbook["_id"])
-#         timestamp = objectid.generation_time#objectID값을 이용하여 타임스탬프 값 얻기
-#         korea_timezone = timedelta(hours=9) #UTC와 한국의 시차
-#         timestamp_kr = timestamp + korea_timezone
-#         formatted_timestamp = timestamp_kr.strftime("%Y.%m.%d %H:%M:%S")
-#         guestbook["timestamp"] = formatted_timestamp
-#     # 지정된 name의 방명록 데이터 모두 가져오기 ,'_id': 가져옴 - 기본값 (true)
-#     # "_id"값을 html로 넘겨주면 방명록 데이터가 정상 조회되지 않음
-#     return jsonify({'result': all_guestbook})
-    
-# #방명록 작성하기    
-# @app.route('/guestbook',methods=['POST'])
-# def guestbook_add():
-#     name_receive = request.form['name_give']
-#     comment_receive = request.form['comment_give']
-#     id_receive = request.form['id_give']
-#     password_receive = request.form['password_give']
-#     print(name_receive,comment_receive,id_receive,password_receive)
-#     doc = {'name' : name_receive,
-#            'comment' : comment_receive,
-#            'id' : id_receive,
-#            'password' : password_receive
-#            }
-#     db.guestbook.insert_one(doc)
-#     return jsonify({'result':'success', 'msg': '댓글 저장이 완료되었습니다.'})
-  
-
-###########################################################################
-# woojin
-
-
-
-############################################################################################################
-# 박수빈 : 메인 프론트엔드
-
-# @app.route("/guestbook", methods=["POST"])
-# def guestbook_post():
-    
-#     name_receive = request.form['name_give']
-#     id_receive = request.form['id_give']
-#     password_receive = request.form['password_give']
-#     comment_receive = request.form['comment_give']
-#     doc = {
-#         'name' : name_receive,
-#         'id' : id_receive,
-#         'password' : password_receive,
-#         'comment' : comment_receive
-#     }
-#     db.guestbook.insert_one(doc)
-
-#     return jsonify({'msg': '저장 완료!'})
-
-# @app.route("/guestbook", methods=["GET"])
-# def guestbook_get():
-#     all_comments = list(db.guestbook.find({}, {'_id': False}))
-#     return jsonify({'result': all_comments})
-#############################################################################################
-#subin
-
-#############################################################################################
 #이경원 : 박명록 작성하기
 @app.route('/guestbook', methods=['POST'])
 def guestbook_post():
@@ -200,84 +130,7 @@ def guestbook_get():
         guestbook["_id"]=str(guestbook["_id"])
     return jsonify({'result':all_guestbook})
 
-# @app.route("/savecomment", methods=["POST"])
-# def savecomment_post():
 
-#     count = list(db.guestbook.find({}, {'_id':False})) #db 데이터 수를 카운트
-#     num = len(count) + 1  # 카운트 한 수에 1을 더해서 새로운 데이터 카운트
-
-#     # 화면에서 넘어 오는 값
-#     id_receive = request.form['id_give']
-#     pw_receive = request.form['pw_give']
-#     name_receive = request.form['name_give']
-#     comment_receive = request.form['comment_give']
-
-#     doc = {
-#     'id':id_receive,
-#     'pw':pw_receive,
-#     'name':name_receive,
-#     'comment':comment_receive,
-#     'num' : num
-# }
-    
-    # headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    # data = requests.get(url_receive,headers=headers)
-
-    # soup = BeautifulSoup(data.text, 'html.parser')
-
-    # # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
-    # ogtitle = soup.select_one('meta[property="og:title"]')['content']
-    # ogdesc = soup.select_one('meta[property="og:description"]')['content']
-    # ogimage = soup.select_one('meta[property="og:image"]')['content']
-
-    # db.guestbook.insert_one(doc)
-    
-    # return jsonify({'msg':'방명록 저장완료!'})
-
-
-
-# @app.route("/deletecomment", methods=["POST"])
-# def deletecomment_post():
-
-#     delete_receive = request.form["num_give"]
-#     # logging.warn("loglog"+ delete_receive)
-    
-#     db.guestbook.delete_one({'num': int(delete_receive)})
-#     return jsonify({'msg':'방명록 삭제완료!'})
-
-
-
-# @app.route("/check", methods=["POST"])
-# def check_post():
-
-#     pw_receive = request.form["pw_give"]
-#     num_receive = request.form["num_give"]
-
-#     print("전달 받은 패스워드 : "+ pw_receive+ "디비 넘버"+ int(num_receive))
-
-#     a = list(db.guestbook.find({'pw':pw_receive, 'num':int(num_receive)},{'_id':False}))
-#     b = list(db.guestbook.find({'num':int(num_receive)},{'_id':False}))
-
-#     for i in a:
-#         print(i)
-
-
-#     logging.warn("")
-
-
-#     for i in b:
-#         print(i)
-
-    
-#     # db.guestbook.delete_one({'num': int(delete_receive)})
-#     return jsonify({'msg':'본인 확인 완료!'})
-
-
-
-# @app.route("/guestbook", methods=["GET"])
-# def guestbook_get():
-#     guestbook = list(db.guestbook.find({},{'_id':False}))
-#     return jsonify({'result':guestbook})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
